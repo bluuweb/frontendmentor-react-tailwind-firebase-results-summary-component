9b129738b66a6741790979be5982a7a7c7b84b11
bluuweb <contacto@bluuweb.cl>
2023-03-22 16:59:27 -0300

    of 100

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -8,7 +8,7 @@ export default function CardResult({countdown} : Props): JSX.Element {
       <h3 className="pb-4 text-gray-300">Tiempo restante</h3>
       <div className="mx-auto grid h-40 w-40 place-content-center rounded-full bg-gradient-to-b from-violet-900 to-transparent">
         <span className="text-5xl font-bold text-white">{countdown}</span>
-        <span className="mt-2 text-sm text-gray-300">of 100</span>
+        {/* <span className="mt-2 text-sm text-gray-300">of 100</span> */}
       </div>
       <h2 className="my-4 text-xl font-medium text-white">{countdown > 0 ? 'Espera...' : 'Puedes votar'}</h2>
       <p className="text-gray-400">
